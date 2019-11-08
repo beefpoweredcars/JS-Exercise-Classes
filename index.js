279d0be4dd7524259f10aa32767fbbb12a02a293
Greg Cushing <beefpoweredcars@yahoo.com>
2019-11-07 19:16:26 -0500

    come back and do math bruh

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ class Car {
    const newTank = gallons + this.tank;
    this.tank = newTank;
   }
+
 }
 
 /*
@@ -99,7 +100,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(obj){
+    this.name = obj.name;
+    this.age = obj.age;
+    this.location = obj.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
 }
 
 /*
